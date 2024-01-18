e8f2774a016fc67f69c5d5cef613ed21e93f9f76
Tomas Cosentino <tomascosentino123@gmail.com>
2024-01-18 15:19:44 -0300

    desafio 1 completado

--- ProductManager.js
+++ ProductManager.js
@@ -6,28 +6,77 @@ class ProductManager {
     }
     addProduct(producto){
         //VALIDAR QUE TODOS LOS DATOS SE HAYAN INGRESADO
+        const requiredFields = ['title', 'description', 'price', 'thumbnail', 'code', 'stock']
+        const missingFields = requiredFields.filter(field => !producto[field])
 
-        const index = this.products.findIndex(prod = prod.code === producto.code)
-        const existe = this.products.includes(prod => prod.code === producto.code)
+        if(missingFields.length > 0) {
+            return console.log(`Faltan campos obligatorios: ${missingFields.join(', ')}`)
+        }
+        //validar que no se repita el campo "code"
+        const existe = this.products.some(prod => prod.code === producto.code)
+
         //existe el producto en el array
-        if(index != -1){
+        if(existe){
             return console.log("Producto ya existente");
         }else{
             producto.id = crypto.randomBytes(10).toString('hex')
             this.products.push(producto)
+            console.log("Producto agregado satisfactoriamente: ", producto)
         }
     }
-    getProducts(producto){
+    getProducts(){
         //devolver arreglo con productos
-        return []
+        return this.products
     }
     getProductById(id){
-        const index = this.products.findIndex(prod => prod.code === id.code)
-        if(index != -1){
-            return console.log("Not Found")
+        //buscar en el arreglo el producto que coincida con el id
+        const existe = this.products.find(prod => prod.code === id)
+
+        if(existe){
+            console.log("Producto Encontrado: ", existe)
         }else{
-            return console.log("Founded")
+            console.log("Not Found")
         }
 
     }
 }
+
+const manager = new ProductManager();
+
+//Test 1
+console.log("Test 1:")
+console.log(manager.getProducts());
+
+//Test 2
+console.log("Test 2:")
+manager.addProduct({
+    title: "producto prueba",
+    description: "este es un producto prueba",
+    price: 200,
+    thumbnail: "sin imagen",
+    code: "abc123",
+    stock: 25,
+});
+
+//Test 3 
+console.log("Test 3:")
+console.log(manager.getProducts());
+
+//Test 4
+console.log("Test 4:")
+manager.addProduct({
+    title: "producto prueba",
+    description: "este es un producto prueba",
+    price: 200,
+    thumbnail: "sin imagen",
+    code: "abc123",
+    stock: 25,
+});
+
+//Test 5 
+console.log("Test 5:")
+manager.getProductById("producto_no_existente");
+
+//Test 6
+console.log("Test 6:")
+manager.getProductById("abc123")
