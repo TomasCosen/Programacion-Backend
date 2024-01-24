5469b1cbff16e29982c823ccb4a8808fafc31e61
Tomas Cosentino <tomascosentino123@gmail.com>
2024-01-24 16:06:54 -0300

    add fs, update addProduct, getProduct y inicio de getProductById

--- ProductManager.js
+++ ProductManager.js
@@ -1,51 +1,68 @@
-import crypto from 'crypto'
+import crypto from "crypto";
+import { promises as fs } from "fs";
 
 class ProductManager {
-    constructor() {
-        this.products = []
-    }
-    addProduct(producto){
-        //VALIDAR QUE TODOS LOS DATOS SE HAYAN INGRESADO
-        const requiredFields = ['title', 'description', 'price', 'thumbnail', 'code', 'stock']
-        const missingFields = requiredFields.filter(field => !producto[field])
+  constructor(path) {
+    this.path = path;
+  }
+  async addProduct(producto) {
+    //VALIDAR QUE TODOS LOS DATOS SE HAYAN INGRESADO
+    const requiredFields = [
+      "title",
+      "description",
+      "price",
+      "thumbnail",
+      "code",
+      "stock",
+    ];
+    const missingFields = requiredFields.filter((field) => !producto[field]);
 
-        if(missingFields.length > 0) {
-            return console.log(`Faltan campos obligatorios: ${missingFields.join(', ')}`)
-        }
-        //validar que no se repita el campo "code"
-        const existe = this.products.some(prod => prod.code === producto.code)
-
-        //existe el producto en el array
-        if(existe){
-            return console.log("Producto ya existente");
-        }else{
-            producto.id = crypto.randomBytes(10).toString('hex')
-            this.products.push(producto)
-            console.log("Producto agregado satisfactoriamente: ", producto)
-        }
+    if (missingFields.length > 0) {
+      return console.log(
+        `Faltan campos obligatorios: ${missingFields.join(", ")}`
+      );
     }
-    getProducts(){
-        //devolver arreglo con productos
-        return this.products
+    const productos = JSON.parse(await fs.readFile(this.path, "utf-8"));
+    const existe = productos.findIndex((prod) => prod.code == producto.code);
+    //existe el producto en el array
+    if (existe != -1) {
+      productos[existe].stock += producto.stock;
+      console.log(
+        "Productos agregados al stock existente, stock: ",
+        productos[existe].stock
+      );
+    } else {
+      producto.id = crypto.randomBytes(10).toString("hex");
+      productos.push(producto);
+      console.log("Producto agregado satisfactoriamente: \n", producto);
     }
-    getProductById(id){
-        //buscar en el arreglo el producto que coincida con el id
-        const existe = this.products.find(prod => prod.code === id)
+    await fs.writeFile(this.path, JSON.stringify(productos));
+  }
+  async getProducts() {
+    //devolver arreglo con productos
+    const productos = JSON.parse(await fs.readFile(this.path, "utf-8"));
+    return productos;
+  }
+  async getProductById(id) {
+    //buscar en el arreglo el producto que coincida con el id
+    const existe = this.products.find((prod) => prod.code === id);
 
-        if(existe){
-            console.log("Producto Encontrado: ", existe)
-        }else{
-            console.log("Not Found")
-        }
-
+    if (existe) {
+      console.log("Producto Encontrado: ", existe);
+    } else {
+      console.log("Not Found");
     }
+  }
 }
+const manager = new ProductManager("./productos.json");
 
-const manager = new ProductManager();
-
+/*
 //Test 1
-console.log("Test 1:")
-console.log(manager.getProducts());
+console.log("Test 1:");
+manager.getProducts()
+    .then((productos) => {
+        console.log("Productos: ", productos);
+    })
 
 //Test 2
 console.log("Test 2:")
@@ -60,7 +77,10 @@ manager.addProduct({
 
 //Test 3 
 console.log("Test 3:")
-console.log(manager.getProducts());
+manager.getProducts()
+    .then((productos) => {
+        console.log("Productos: ", productos);
+    })
 
 //Test 4
 console.log("Test 4:")
@@ -79,4 +99,4 @@ manager.getProductById("producto_no_existente");
 
 //Test 6
 console.log("Test 6:")
-manager.getProductById("abc123")
+manager.getProductById("abc123") */
