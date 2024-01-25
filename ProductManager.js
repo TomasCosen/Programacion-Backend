0e5c189477e70543686b1117b06326ea3c9ab1e5
Tomas Cosentino <tomascosentino123@gmail.com>
2024-01-25 16:03:26 -0300

    desafío completado, add getProductById, updateProduct & deleteProduct

--- ProductManager.js
+++ ProductManager.js
@@ -45,7 +45,8 @@ class ProductManager {
   }
   async getProductById(id) {
     //buscar en el arreglo el producto que coincida con el id
-    const existe = this.products.find((prod) => prod.code === id);
+    const productos = JSON.parse(await fs.readFile(this.path, "utf-8"));
+    const existe = productos.find((prod) => prod.code === id);
 
     if (existe) {
       console.log("Producto Encontrado: ", existe);
@@ -53,50 +54,96 @@ class ProductManager {
       console.log("Not Found");
     }
   }
+  async updateProduct(id, campo) {
+    const productos = JSON.parse(await fs.readFile(this.path, "utf-8"));
+    const indice = productos.findIndex((prod) => prod.code === id);
+
+    if (indice != -1) {
+      productos[indice] = {
+        ...productos[indice],
+        ...campo,
+      };
+      await fs.writeFile(this.path, JSON.stringify(productos));
+      console.log("Producto actualizado: ", productos[indice]);
+    } else {
+      console.log("Not Found");
+    }
+  }
+  async deleteProduct(id) {
+    const productos = JSON.parse(await fs.readFile(this.path, "utf-8"));
+    const indice = productos.findIndex((prod) => prod.code === id);
+
+    if (indice != -1) {
+      productos.splice(indice, 1);
+      await fs.writeFile(this.path, JSON.stringify(productos));
+      console.log("Producto Eliminado");
+    } else {
+      console.log("Not Found");
+    }
+  }
 }
+
 const manager = new ProductManager("./productos.json");
 
-/*
-//Test 1
+/* //Test 1
 console.log("Test 1:");
-manager.getProducts()
-    .then((productos) => {
-        console.log("Productos: ", productos);
-    })
+manager.getProducts().then((productos) => {
+  console.log("Productos: ", productos);
+});
 
 //Test 2
-console.log("Test 2:")
+console.log("Test 2:");
 manager.addProduct({
-    title: "producto prueba",
-    description: "este es un producto prueba",
-    price: 200,
-    thumbnail: "sin imagen",
-    code: "abc123",
-    stock: 25,
+  title: "producto prueba",
+  description: "este es un producto prueba",
+  price: 200,
+  thumbnail: "sin imagen",
+  code: "abc123",
+  stock: 10,
 });
 
-//Test 3 
-console.log("Test 3:")
-manager.getProducts()
-    .then((productos) => {
-        console.log("Productos: ", productos);
-    })
+//Test 3
+console.log("Test 3:");
+manager.getProducts().then((productos) => {
+  console.log("Productos: ", productos);
+});
 
 //Test 4
-console.log("Test 4:")
+console.log("Test 4:");
 manager.addProduct({
-    title: "producto prueba",
-    description: "este es un producto prueba",
-    price: 200,
-    thumbnail: "sin imagen",
-    code: "abc123",
-    stock: 25,
+  title: "producto prueba",
+  description: "este es un producto prueba",
+  price: 200,
+  thumbnail: "sin imagen",
+  code: "abc123",
+  stock: 25,
 });
 
-//Test 5 
-console.log("Test 5:")
+//Test 5
+console.log("Test 5:");
 manager.getProductById("producto_no_existente");
 
 //Test 6
-console.log("Test 6:")
-manager.getProductById("abc123") */
+console.log("Test 6:");
+manager.getProductById("abc123");
+
+//Test 7
+console.log("Test 7:");
+manager.updateProduct("abc123", { price: 250 });
+
+//Test 8
+console.log("Test 8:");
+manager.getProducts().then((productos) => {
+  console.log("Productos: ", productos);
+});
+
+//Test 9
+console.log("Test 9:");
+manager.deleteProduct("abc123");
+
+//Test 10
+console.log("Test 10:");
+manager.getProducts().then((productos) => {
+  console.log("Productos: ", productos);
+});
+*/
